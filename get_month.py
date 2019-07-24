c4d6bb86fbd35cce4d7026b4b69423019933fdf4
Artem <boniekee@gmail.com>
2019-07-24 10:27:52 +0300

    - какое-то говно не работает жопа жопа надо исправить

--- get_month.py
+++ get_month.py
@@ -1,15 +1,24 @@
 import re
 import os
-import get_disks
-import VIDEO_check
+from get_disks import get_disks
+#from VIDEO_check import video
 
 '''
-Определяем блоки по месяцам
+Вычитаем из списка нужный месяц
 '''
+
 def get_month():
-    m = input('Введите номер месяца в двузначном формате: ')
-    # m = re.findall('...' + m + '......',VIDEO_check.VIDEO_check())
-    for mm in VIDEO_check.VIDEO_check():
-        month = re.match(r'...' + m + '......', mm)
-        print('Обнаружены следующие объекты: ', month, end = '\n')
-    return(month)
+    video = ['01-01-19 00', '02-02-19 01', '03-03-19 02', '04-04-19 03', '02-01-19 03']
+    video_str = ', '.join(video)
+    print(video_str)
+    # needed = []
+    date =  input('Enter date: ')
+    date = "'..." + date + "...'"
+    for m in video_str:
+        # result = re.findall(r'...01-19...', video_str)
+        result = re.findall(date, video_str)
+    for d in result:
+        video = video.remove(d)
+    return(result) 
+    print(video)
+get_month()
